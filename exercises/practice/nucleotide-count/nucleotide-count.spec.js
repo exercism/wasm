159d8c2cd5c587e7bda9ba16b33e3ed713773499
Sean McBride <spmcbride1201@gmail.com>
2022-02-19 18:16:17 -0500

    feat: nucleotide count ESM

--- exercises/practice/nucleotide-count/nucleotide-count.spec.js
+++ exercises/practice/nucleotide-count/nucleotide-count.spec.js
@@ -1,14 +1,12 @@
-import { compileWat } from './compile-wat';
-import {TextEncoder} from 'util';
+import { compileWat, WasmRunner } from "wasm-lib";
 
 let wasmModule;
 let currentInstance;
-let linearMemory;
-let textEncoder = new TextEncoder('utf8');
 
 beforeAll(async () => {
   try {
-    const {buffer} = await compileWat("nucleotide-count.wat", {multi_value: true});
+    const watPath = new URL("./nucleotide-count.wat", import.meta.url);
+    const { buffer } = await compileWat(watPath);
     wasmModule = await WebAssembly.compile(buffer);
   } catch (err) {
     console.log(`Error compiling *.wat: ${err}`);
@@ -16,25 +14,29 @@ beforeAll(async () => {
   }
 });
 
-function countNucleotides(input){
-  const inputOffset = 64;
-  const inputBuffer = new Uint8Array(linearMemory.buffer, inputOffset, input.length);
-  textEncoder.encodeInto(input, inputBuffer);
+function countNucleotides(input) {
+  const inputBufferOffset = 64;
+  const inputBufferCapacity = 256;
+
+  const inputLengthEncoded = new TextEncoder().encode(input).length;
+  if (inputLengthEncoded > inputBufferCapacity) {
+    throw new Error("String is too large for buffer of size 128 bytes");
+  }
+
+  currentInstance.set_mem_as_utf8(inputBufferOffset, inputLengthEncoded, input);
 
   // Pass offset and length to WebAssembly function
   return currentInstance.exports.countNucleotides(inputOffset, input.length);
 }
 
-
-describe('count all nucleotides in a strand', () => {
+describe("count all nucleotides in a strand", () => {
   beforeEach(async () => {
     currentInstance = null;
     if (!wasmModule) {
       return Promise.reject();
     }
     try {
-      linearMemory = new WebAssembly.Memory({initial: 1});
-      currentInstance = await WebAssembly.instantiate(wasmModule, {env: {linearMemory}});
+      currentInstance = await new WasmRunner(wasmModule);
       return Promise.resolve();
     } catch (err) {
       console.log(`Error instantiating WebAssembly module: ${err}`);
@@ -42,27 +44,27 @@ describe('count all nucleotides in a strand', () => {
     }
   });
 
-  test('empty strand', () => {
-    expect(countNucleotides('')).toEqual([0, 0, 0, 0]);
+  test("empty strand", () => {
+    expect(countNucleotides("")).toEqual([0, 0, 0, 0]);
   });
 
-  xtest('can count one nucleotide in single-character input', () => {
-    expect(countNucleotides('G')).toEqual([0, 0, 1, 0]);
+  xtest("can count one nucleotide in single-character input", () => {
+    expect(countNucleotides("G")).toEqual([0, 0, 1, 0]);
   });
 
-  xtest('strand with repeated nucleotide', () => {
-    expect(countNucleotides('GGGGGGG')).toEqual([0, 0, 7, 0]);
+  xtest("strand with repeated nucleotide", () => {
+    expect(countNucleotides("GGGGGGG")).toEqual([0, 0, 7, 0]);
   });
 
-  xtest('strand with multiple nucleotides', () => {
+  xtest("strand with multiple nucleotides", () => {
     expect(
       countNucleotides(
-        'AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'
+        "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
       )
     ).toEqual([20, 12, 17, 21]);
   });
 
-  xtest('strand with invalid nucleotides', () => {
-    expect(countNucleotides('AGXXACT')).toEqual([-1, -1, -1, -1]);
+  xtest("strand with invalid nucleotides", () => {
+    expect(countNucleotides("AGXXACT")).toEqual([-1, -1, -1, -1]);
   });
 });
