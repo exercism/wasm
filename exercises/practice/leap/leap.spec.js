3fbbe2cbbc502f1bdd1fbec46a53b2eb1fd6801c
Sean McBride <spmcbride1201@gmail.com>
2022-02-19 17:44:18 -0500

    feat: leap ESM

--- exercises/practice/leap/leap.spec.js
+++ exercises/practice/leap/leap.spec.js
@@ -1,18 +1,20 @@
-import { compileWat } from './compile-wat';
+import { compileWat, WasmRunner } from "wasm-lib";
 
 let wasmModule;
 let currentInstance;
 
 beforeAll(async () => {
   try {
-    const {buffer} = await compileWat("leap.wat", {multi_value: true});
+    const watPath = new URL("./leap.wat", import.meta.url);
+    const { buffer } = await compileWat(watPath);
     wasmModule = await WebAssembly.compile(buffer);
   } catch (err) {
     console.log(`Error compiling *.wat: ${err}`);
+    process.exit(1);
   }
 });
 
-describe('A leap year', () => {
+describe("A leap year", () => {
   beforeEach(async () => {
     currentInstance = null;
 
@@ -20,7 +22,7 @@ describe('A leap year', () => {
       return Promise.reject();
     }
     try {
-      currentInstance = await WebAssembly.instantiate(wasmModule);
+      currentInstance = await new WasmRunner(wasmModule);
       return Promise.resolve();
     } catch (err) {
       console.log(`Error instantiating WebAssembly module: ${err}`);
@@ -28,39 +30,39 @@ describe('A leap year', () => {
     }
   });
 
-  test('year not divisible by 4 in common year', () => {
+  test("year not divisible by 4 in common year", () => {
     expect(currentInstance.exports.isLeap(2015)).toBe(0);
   });
 
-  xtest('year divisible by 2, not divisible by 4 in common year', () => {
+  xtest("year divisible by 2, not divisible by 4 in common year", () => {
     expect(currentInstance.exports.isLeap(1970)).toBe(0);
   });
 
-  xtest('year divisible by 4, not divisible by 100 in leap year', () => {
+  xtest("year divisible by 4, not divisible by 100 in leap year", () => {
     expect(currentInstance.exports.isLeap(1996)).toBe(1);
   });
 
-  xtest('year divisible by 4 and 5 is still a leap year', () => {
+  xtest("year divisible by 4 and 5 is still a leap year", () => {
     expect(currentInstance.exports.isLeap(1960)).toBe(1);
   });
 
-  xtest('year divisible by 100, not divisible by 400 in common year', () => {
+  xtest("year divisible by 100, not divisible by 400 in common year", () => {
     expect(currentInstance.exports.isLeap(2100)).toBe(0);
   });
 
-  xtest('year divisible by 100 but not by 3 is still not a leap year', () => {
+  xtest("year divisible by 100 but not by 3 is still not a leap year", () => {
     expect(currentInstance.exports.isLeap(1900)).toBe(0);
   });
 
-  xtest('year divisible by 400 is leap year', () => {
+  xtest("year divisible by 400 is leap year", () => {
     expect(currentInstance.exports.isLeap(2000)).toBe(1);
   });
 
-  xtest('year divisible by 400 but not by 125 is still a leap year', () => {
+  xtest("year divisible by 400 but not by 125 is still a leap year", () => {
     expect(currentInstance.exports.isLeap(2400)).toBe(1);
   });
 
-  xtest('year divisible by 200, not divisible by 400 is common year', () => {
+  xtest("year divisible by 200, not divisible by 400 is common year", () => {
     expect(currentInstance.exports.isLeap(1800)).toBe(0);
   });
 });
