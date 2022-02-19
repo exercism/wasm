7ee147bead017404d32a47999dfcbd5599385aae
Sean McBride <spmcbride1201@gmail.com>
2022-02-19 18:34:20 -0500

    fix: Correct nucleotide count

--- exercises/practice/nucleotide-count/nucleotide-count.spec.js
+++ exercises/practice/nucleotide-count/nucleotide-count.spec.js
@@ -20,13 +20,18 @@ function countNucleotides(input) {
 
   const inputLengthEncoded = new TextEncoder().encode(input).length;
   if (inputLengthEncoded > inputBufferCapacity) {
-    throw new Error("String is too large for buffer of size 128 bytes");
+    throw new Error(
+      `String is too large for buffer of size ${inputBufferCapacity} bytes`
+    );
   }
 
   currentInstance.set_mem_as_utf8(inputBufferOffset, inputLengthEncoded, input);
 
   // Pass offset and length to WebAssembly function
-  return currentInstance.exports.countNucleotides(inputOffset, input.length);
+  return currentInstance.exports.countNucleotides(
+    inputBufferOffset,
+    input.length
+  );
 }
 
 describe("count all nucleotides in a strand", () => {
@@ -48,15 +53,15 @@ describe("count all nucleotides in a strand", () => {
     expect(countNucleotides("")).toEqual([0, 0, 0, 0]);
   });
 
-  xtest("can count one nucleotide in single-character input", () => {
+  test("can count one nucleotide in single-character input", () => {
     expect(countNucleotides("G")).toEqual([0, 0, 1, 0]);
   });
 
-  xtest("strand with repeated nucleotide", () => {
+  test("strand with repeated nucleotide", () => {
     expect(countNucleotides("GGGGGGG")).toEqual([0, 0, 7, 0]);
   });
 
-  xtest("strand with multiple nucleotides", () => {
+  test("strand with multiple nucleotides", () => {
     expect(
       countNucleotides(
         "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
@@ -64,7 +69,7 @@ describe("count all nucleotides in a strand", () => {
     ).toEqual([20, 12, 17, 21]);
   });
 
-  xtest("strand with invalid nucleotides", () => {
+  test("strand with invalid nucleotides", () => {
     expect(countNucleotides("AGXXACT")).toEqual([-1, -1, -1, -1]);
   });
 });
